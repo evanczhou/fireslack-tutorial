3fb2c075395a50884aebbdb8eadfa9254c90fdfe
Evan <evanczhou@gmail.com>
2015-09-10 17:54:30 -0700

    first commit

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('angularfireSlackApp')
+  .controller('AuthCtrl', function(Auth, $state){
+    var authCtrl = this;
+
+    console.log(Auth);
+    
+    authCtrl.user = {
+      email: '',
+      password: ''
+    };
+
+    authCtrl.login = function (){
+      Auth.$authWithPassword(authCtrl.user).then(function (auth){
+        $state.go('home');
+      }, function (error){
+        authCtrl.error = error;
+      });
+    };
+
+    authCtrl.register = function (){
+      Auth.$createUser(authCtrl.user).then(function (user){
+        authCtrl.login();
+      }, function (error){
+        authCtrl.error = error;
+      })
+    }
+  });
